c924a4b42e79faaee824c36ff2512ba98e5147fb
Jack Daniel Kinne <bravelemming@gmail.com>
2019-07-10 10:47:21 -0700

    weather api

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const superagent = require('superagent');
 
 const PORT = process.env.PORT;
 const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const app = express();
 app.use(cors());
@@ -34,7 +35,66 @@ function searchToLatLng(request, response) {
     })
 }
 
+//TODO: superagent path for searching the weather with an API
+app.get('/weather', searchWeather);
+//weather constructor
+function Weather(forcast, time) {
+  this.forcast = forcast;
+  this.time = time
+}
+//function to search the weather
+function searchWeather(request, response) {
+  const locationName = request.query.data; //location user entered
+  console.log('location name: ', locationName);
+  //google maps api 
+  const url = `https://api.darksky.net/forecast/${WEATHER_API_KEY}/37.8267,-122.4233`;
 
+  superagent.get(url) //superagent api request
+    .then (result => { //promise on async
+
+      console.log('result: ', result);
+
+      // let location = { //object
+      //   search_query: locationName,
+      //   formatted_query : result.body.results[0].formatted_address,
+      //   latitude : result.body.results[0].geometry.location.lat,
+      //   longitude : result.body.results[0].geometry.location.lng
+      // }
+      // response.send(location); //send to user
+    }).catch(e => {
+      //if errors
+      console.error(e);
+      response.status(500).send('status 500: things are wrong.');
+    })
+}
+//THE COPY ENDS
+
+//the function calls
+
+function searchWeather(location) {
+  //currently grabbing from a json file.
+  const weatherData = require('./data/darksky.json');
+  let res = []
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+  weatherData.daily.data.forEach((el) => {
+    //https://stackoverflow.com/questions/4631928/convert-utc-epoch-to-local-date
+    let utcSeconds = el.time;
+    let date = new Date(0);
+    date.setUTCSeconds(utcSeconds);
+    console.log(date);
+    //can also use .toDateString()
+    let weather = new Weather(el.summary, date.toLocaleDateString('en-US', options));
+    res.push(weather);
+  });
+  return res;
+}
+//end the function calls
+
+
+
+
+//TODO: delete this later
 app.get('/weather', (req, res) => {
   try {
     // console.log(req.query.location)
@@ -59,29 +119,7 @@ function Location(locationName, formatted_address, lat, lng) {
 }
 
 
-function Weather(forcast, time) {
-  this.forcast = forcast;
-  this.time = time
-}
-// eslint-disable-next-line no-unused-vars
-function searchWeather(location) {
-  //currently grabbing from a json file.
-  const weatherData = require('./data/darksky.json');
-  let res = []
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-  weatherData.daily.data.forEach((el) => {
-    //https://stackoverflow.com/questions/4631928/convert-utc-epoch-to-local-date
-    let utcSeconds = el.time;
-    let date = new Date(0);
-    date.setUTCSeconds(utcSeconds);
-    console.log(date);
-    //can also use .toDateString()
-    let weather = new Weather(el.summary, date.toLocaleDateString('en-US', options));
-    res.push(weather);
-  });
-  return res;
-}
 
 
 
